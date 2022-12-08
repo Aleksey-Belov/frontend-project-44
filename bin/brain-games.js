dae9f90b1b7bb62a73a0d4de32fb9bc8da0fc288
Aleksey-Belov <asse.com97@gmail.com>
2022-12-08 07:43:00 +0300

    Revert "add play brain-even"
    This reverts commit c9ffea15633e0714c7f52b6f83bdb9e6ea1d910b.

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,3 @@
 #!/usr/bin/env node
 
-import requestName from '../src/cli.js';
-
-console.log('Welcome to the Brain Games!');
-requestName();
+console.log("Welcome to the Brain Games!");
--- src/cli.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const requestName = () => {
-  const yourName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${yourName}!`);
-};
-
-export default requestName;
